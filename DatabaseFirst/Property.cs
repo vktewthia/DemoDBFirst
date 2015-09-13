18a8e18f0f406d2a79aa847cb0fbb3eda8de4469
Vinod Tewthia <TewthiaV@advisory.com>
2015-09-13 23:52:07 +0530

    Removed the unwanted property

--- DatabaseFirst/Property.cs
+++ DatabaseFirst/Property.cs
@@ -25,7 +25,6 @@ namespace DatabaseFirst
         public int PropertyNo { get; set; }
         public string PropertyCaption { get; set; }
         public string PropertyId { get; set; }
-        public string AreaCode { get; set; }
     
         public virtual ICollection<PropertyMeterMapping> PropertyMeterMappings { get; set; }
         public virtual ICollection<RentalProperty> RentalProperties { get; set; }
